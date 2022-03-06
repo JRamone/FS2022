190c6fa9a0711c41e8b6d72ffdc1fb6154f41b6a
JRamone <JMKoho@gmail.com>
2022-03-07 01:33:55 +0200

    lisää 2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,17 +1,15 @@
 
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter,setFilter] = useState('')
-
+const Filter = ({filter, setFilter}) => {
+  return (
+    <>
+    filter shown with 
+      <input onChange={(e) => setFilter(e.target.value.toLowerCase())}></input>
+    </>
+  )
+}
+const PersonForm = ({persons,setPersons,newName, newNumber,setNewName,setNewNumber}) => {
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.map(p => p.name).includes(newName)){
@@ -23,13 +21,9 @@ const App = () => {
     setNewNumber('')
     console.log('hello');
   }
-
-  return (
-    <div>
-      filter shown with 
-      <input onChange={(e) => setFilter(e.target.value.toLowerCase())}></input>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
+  return(
+  <>
+  <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
         </div>
@@ -40,10 +34,37 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+  </>
+  )
+}
+const Persons = ({persons,filter}) => {
+  return(
+    <>
       <ul style={{listStyleType : 'none'}}>
         {persons.filter(p => p.name.toLowerCase().includes(filter)).map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter,setFilter] = useState('')
+
+  return (
+    <div>
+      <Filter filter={filter} setFilter={setFilter}/>
+      <h2>Phonebook</h2>
+      <PersonForm persons={persons} setPersons={setPersons} newName={newName} newNumber={newNumber} setNewName={setNewName} setNewNumber={setNewNumber}/>
+      <h2>Numbers</h2>
+      <Persons persons={persons} filter={filter}/>
     </div>
   )
 
