284cef960dab037d9bd1396fb7933d6b0e9d3a43
JRamone <JMKoho@gmail.com>
2022-03-06 10:45:53 +0200

    lisää 2.5

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,4 +1,6 @@
 
+import {Content,Course,Part,Header,Total} from './components/components'
+
 const App = () => {
 
   const courses = [
@@ -50,46 +52,6 @@ const App = () => {
     <div>
       {courses.map(course => <Course key={course.id} course = {course}/>)}
     </div>
-  )
-}
-const Content = ({parts}) => {
-  //console.log(props)
-  return (
-    <>
-      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises}/>) }
-    </>
-    
-  )
-}
-
-const Course = ({course}) => {
-  //console.log(course)
-  return (
-    <>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </>
-  )
-}
-
-const Part = ({name, exercises}) => {
-  return (
-    <>
-    <p>{name} {exercises}</p>
-    </>
-  )
-}
-const Header = ({name}) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Total = ({parts}) => {
-  console.log(parts)
-  return (
-    <p style={{fontWeight : 'bold'}}>Total of {parts.map(part => part.exercises).reduce((a,b) => a+b,0)} exercises</p>
   )
 }
 
