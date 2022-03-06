898377fdc575b7bc2f8fb7f509ce8e7a2b3fe4ce
JRamone <JMKoho@gmail.com>
2022-03-06 23:45:59 +0200

    lisää 2.6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,10 +1,37 @@
 
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name : newName}))
+    setNewName('')
+    console.log('hello');
+  }
+
   return (
-    <>
-      hello
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={{listStyleType : 'none'}}>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
   )
+
 }
 
-export default App;
+export default App
--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,4 +1,7 @@
 
+import App from './App'
+import ReactDOM from 'react-dom'
+
 ReactDOM.render(
     <App />,
   document.getElementById('root')
