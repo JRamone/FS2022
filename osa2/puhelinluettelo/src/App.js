498abf13fa34cb00b1bec150c0668c5130320cc4
JRamone <JMKoho@gmail.com>
2022-03-07 00:47:18 +0200

    lisää 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,10 +3,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter,setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -22,6 +26,8 @@ const App = () => {
 
   return (
     <div>
+      filter shown with 
+      <input onChange={(e) => setFilter(e.target.value.toLowerCase())}></input>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
@@ -36,7 +42,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul style={{listStyleType : 'none'}}>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {persons.filter(p => p.name.toLowerCase().includes(filter)).map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
