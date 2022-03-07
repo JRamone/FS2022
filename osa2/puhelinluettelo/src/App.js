8654b67ce9b73f8a7ab82487ef7f53853580758e
JRamone <JMKoho@gmail.com>
2022-03-07 20:23:36 +0200

    lisää 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,6 @@
 
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, setFilter}) => {
   return (
@@ -48,12 +49,20 @@ const Persons = ({persons,filter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+
+  useEffect(() => {
+    const handler = (response) => {
+      setPersons(response.data.persons)
+    }
+
+    axios.get('http://localhost:3001/db')
+      .then(handler)
+    
+    
+  }, [])
+  
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter,setFilter] = useState('')
